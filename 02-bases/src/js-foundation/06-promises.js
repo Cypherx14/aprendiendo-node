ba1a5555501be143066441d53d88a15f38ec24c2
Cypher14x <alexcoro14x@gmail.com>
2025-09-15 18:18:56 -0500

    async await

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,12 +1,21 @@
 
-const getPokemonByID = (id, callback ) => {
+// const getPokemonByID = (id, callback ) => {
+//     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+//     return fetch(url)
+//         .then((resp) => {
+//             return resp.json();
+//         })
+//         .then((pokemon) =>  pokemon.name);
+// }
+
+//using async/await
+const getPokemonByID = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
-    return fetch(url)
-        .then((resp) => {
-            return resp.json();
-        })
-        .then((pokemon) =>  pokemon.name);
+    const resp = await fetch(url);
+    const pokemon = await resp.json();
+    return pokemon.name;
 }
+
 
 
 module.exports = {
