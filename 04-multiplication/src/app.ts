0b83b4b466a1ee545a4bd6f2a5f0d9139263e111
Alex <alexcoro14xd@gmail.com>
2025-09-23 23:43:44 -0500

    tarea crear archivo

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,5 +1,24 @@
 
+import * as fs from 'fs';
 
-const message: string = "Hola mundo";
-console.log(message);
+let data:string='';
+const base=5;
+const headerMessage = `TABLA DEL ${base}\n`;
+for (let index = 1; index < 11; index++) {
+    data += `5 x ${index} = ${base * index}\n`;
+}
+console.log(headerMessage);
+console.log(data);
+data = headerMessage + data;
+
+const outputPath = 'outputs/'; 
+
+
+fs.mkdirSync(outputPath, {recursive:true});
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, data);
+console.log('File created');
+
+
+
+
 
