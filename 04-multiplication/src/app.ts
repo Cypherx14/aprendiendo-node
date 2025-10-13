539bb49dd8ded75972073bc4ac5152826a6b10df
Cypher14x <alexcoro14x@gmail.com>
2025-10-13 17:13:13 -0500

    cambios

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,24 +1,5 @@
 
-import * as fs from 'fs';
-
-let data:string='';
-const base=5;
-const headerMessage = `TABLA DEL ${base}\n`;
-for (let index = 1; index < 11; index++) {
-    data += `5 x ${index} = ${base * index}\n`;
-}
-console.log(headerMessage);
-console.log(data);
-data = headerMessage + data;
-
-const outputPath = 'outputs/'; 
 
 
-fs.mkdirSync(outputPath, {recursive:true});
-fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, data);
-console.log('File created');
-
-
-
-
+console.log(process.argv);
 
